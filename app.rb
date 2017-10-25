97c736f75e73b2f3361a670a22fb1e2c6e6812e4
Robert Laws <roblaws@me.com>
2017-10-25 06:13:19 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,10 @@
 class Application < Sinatra::Base
-  # Write your code here!
+  # Old route from previous tests
+  get '/' do
+    erb :index
+  end
 
+  post '/greet' do
+    erb :greet
+  end
 end
